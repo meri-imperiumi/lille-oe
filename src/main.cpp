13060ee64858236d9cea62cf4ad76adcc6533e6a
Susanna Huhtanen <ihmis-suski@Susannas-Air.lan>
2022-12-29 14:59:21 +0100

    rm unnecessary comments and rename SignalK paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-// Signal K application template file.
-//
-// This application demonstrates core SensESP concepts in a very
-// concise manner. You can build and upload the application as is
-// and observe the value changes on the serial port monitor.
-//
-// You can use this source file as a basis for your own projects.
-// Remove the parts that are not relevant to you, and add your own code
-// for external hardware libraries.
-
 #include "sensesp/sensors/digital_input.h"
 #include "sensesp/sensors/sensor.h"
 #include "sensesp/signalk/signalk_output.h"
@@ -27,12 +17,7 @@ void setup() {
   // Construct the global SensESPApp() object
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
-                    // Set a custom hostname for the app.
                     ->set_hostname("boat-remote")
-                    // Optionally, hard-code the WiFi and Signal K server
-                    // settings. This is normally not needed.
-                    //->set_wifi("My WiFi SSID", "my_wifi_password")
-                    //->set_sk_server("192.168.10.3", 80)
                     ->get_app();
 
 
@@ -40,33 +25,20 @@ void setup() {
   // Read GPIO 14 every time it changes
   // Board G14
 
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
   const uint8_t kDigitalInputAPin = 14;
   const unsigned int kDigitalInputAInterval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
   pinMode(kDigitalInputAPin, INPUT_PULLDOWN);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
 
   auto* digital_inputA = new RepeatSensor<bool>(
       kDigitalInputAInterval,
       [kDigitalInputAPin]() { return digitalRead(kDigitalInputAPin); });
 
-  // Connect digital input 2 to Signal K output.
+  // Connect digital input A to Signal K output.
   digital_inputA->connect_to(new SKOutputBool(
-      "sensors.digital_inputA.value",          // Signal K path
-      "/sensors/digital_inputA/value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Digital input A value")  // Value description
+      "electrical.switches.remoteA.state",              // Signal K path
+      "/electrical/switches_remoteA/state",             // configuration path
+      new SKMetadata("",                                // No units for boolean values
+                     "Electical switch remote A state") // Value description
       ));
 
 
@@ -75,22 +47,9 @@ void setup() {
   // Read GPIO 25 every time it changes
   // Board G25
 
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
   const uint8_t kDigitalInputBPin = 25;
   const unsigned int kDigitalInputBInterval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
   pinMode(kDigitalInputBPin, INPUT_PULLDOWN);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
 
   auto* digital_inputB = new RepeatSensor<bool>(
       kDigitalInputBInterval,
@@ -98,10 +57,10 @@ void setup() {
 
   // Connect digital input 2 to Signal K output.
   digital_inputB->connect_to(new SKOutputBool(
-      "sensors.digital_inputB.value",          // Signal K path
-      "/sensors/digital_inputB/value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Digital input B value")  // Value description
+      "electrical.switches.remoteB.state",              // Signal K path
+      "/electrical/switches_remoteB/state",             // configuration path
+      new SKMetadata("",                                // No units for boolean values
+                     "Electical switch remote B state") // Value description
       ));    
 
   // Start networking, SK server connections and other SensESP internals
